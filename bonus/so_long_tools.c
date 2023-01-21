0ca87121fc6e7ac997750308183a42c7897a1e5d
Ayman Msaoub <amsaoub@e1r8p8.1337.ma>
2023-01-21 17:19:01 +0100

    done.1

--- bonus/so_long_tools.c
+++ bonus/so_long_tools.c
@@ -6,7 +6,7 @@
 /*   By: amsaoub <amsaoub@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 11:43:03 by amsaoub           #+#    #+#             */
-/*   Updated: 2023/01/21 15:49:57 by amsaoub          ###   ########.fr       */
+/*   Updated: 2023/01/21 17:18:06 by amsaoub          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -193,8 +193,7 @@ int render_next_frame(t_list *k)
 	render_help(k, i);
 	movesf(k);
 	i++;
-	
-	return (0);
+	return (i);
 	
 }
 
@@ -289,7 +288,6 @@ int	move_fire(t_list *k)
 		}
 		i++;
 	}
-	puts("fire");
 	return (0);
 }
 
@@ -297,7 +295,7 @@ int	movesf(t_list *k)
 {
 	static int i;
 
-	if(i == 8000)
+	if(i == 10000)
 	{
 		i = 0;
 		move_fire(k);
@@ -310,10 +308,11 @@ int	move_fire_h(t_list *k ,int i, int j)
 {
 	int	x;
 	int y;
-	x = rand() % 3 - 1;
-	y = rand() % 3 - 1;
-	
-	if(k->map[i+x][j+y] == 'P')
+	x = (rand() % 3) - 1;
+	y = (rand() % 3) - 1;
+	if(x == y || -x == y)
+		return (0);
+	if(k->map[i+x][j+y] == 'P' || (k->x_p/64 ==j+y && k->y_p/64 == i+x))	
 		lose();
 	if(k->map[i+x][j+y] == '0')
 	{
